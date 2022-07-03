dc4cd65becac72f5c141b9d073d0fee92ae74801
ParkSunJung <ParkSunJung@192.168.1.102>
2022-07-03 19:26:51 +0900

    push test

--- board/src/main/java/board/board/controller/BoardController.java
+++ board/src/main/java/board/board/controller/BoardController.java
@@ -16,7 +16,7 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
-	//게시글 목록
+	// 게시글 목록
 	@RequestMapping("/board/openBoardList.do")
 	public ModelAndView openBoardList() throws Exception{
 		ModelAndView mv = new ModelAndView("/board/boardList");
@@ -27,13 +27,13 @@ public class BoardController {
 		return mv;
 	}
 	
-	//게시글 등록 화면 호출
+	// 게시글 등록 화면 호출
 	@RequestMapping("/board/openBoardWrite.do")
 	public String openBoardWrite() throws Exception {
 		return "/board/boardWrite";
 	}
 	
-	//게시글 등록
+	// 게시글 등록
 	@RequestMapping("/board/insertBoard.do")
 	public String insertBoard(BoardDto board) throws Exception {
 		boardService.insertBoard(board);
